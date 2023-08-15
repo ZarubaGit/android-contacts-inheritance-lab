302ec59d9063ba193fb432dd4021f0afb0fa6b25
Nikita <lifesteel6@gmail.com>
2023-08-15 19:11:06 +0400

    Создан отдельный класс родительский ContactsOrderTypeUi, добавлено наследние в классы SortTypeUI, FilterContactTypeUi

---
+++ app/src/main/java/ru/yandex/practicum/contacts/presentation/base/ContactsOrderTypeUi.java
@@ -0,0 +1,20 @@
+package ru.yandex.practicum.contacts.presentation.base;
+
+import androidx.annotation.NonNull;
+
+ abstract public class ContactsOrderTypeUi {
+
+     protected final String type;
+     protected final boolean isSelected;
+
+    public ContactsOrderTypeUi(@NonNull String type, boolean isSelected) {
+        this.type = type;
+        this.isSelected = isSelected;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+     public abstract String createLogMessage();
+}
